2065570b5b59fd00ca10ac035ed66dc85c108395
MJames8 <129840898+MJames8@users.noreply.github.com>
2023-06-22 09:35:52 +0100

    Add files via upload

---
+++ Autorijden-26cc3219-3901dd21/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace rijbewijs
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            bool check = true;
+            while (check)
+            {
+                Console.WriteLine("Hoe oud ben je?");
+                float leeftijd = float.Parse(Console.ReadLine());
+
+                if (leeftijd < 16.5)
+                {
+                    Console.WriteLine("Je mag nog geen auto rijden!");
+                }
+                else
+                {
+                    Console.WriteLine("Je mag auto rijden!");
+                }
+
+
+
+            }
+        }
+    }
+}
